bbc9ef930cafab12620edb3f20880561c003d1b7
Arkarhein-123 <hein90510@gmail.com>
2026-05-25 17:07:12 +0630

    do revision for first day

---
+++ Revision/01-first-react-app/src/App.jsx
@@ -0,0 +1,27 @@
+//  const welcome = {
+//   greet : "Hello",
+//   title : "React"
+//  }
+function getTitle(title){
+  return title;
+}
+let myArr = [1,2,3,4,5,6,7,8,9,10];
+ function App(){
+  return (
+    <>
+      <h1>Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search</label>
+      <input type="text" id="search" />
+      <button>Search</button>
+
+      <p>My Array Data</p>
+      <ul>
+        {myArr.map((data)=>{
+          return <li key={data}>{data}</li>
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
